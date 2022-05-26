2ce413468a7649092c270f1132cc0925ffe0b8cf
Willian <willsbit@gmail.com>
2022-05-26 19:30:50 -0300

    [FEATURE] Implement From to convert from the ask layer GeClip to the domain GetClip

--- src/lib/data/model.rs
+++ src/lib/data/model.rs
@@ -41,6 +41,14 @@ pub struct GetClip {
     pub(in crate::data) shortcode: String
 }
 
+impl From<crate::service::ask::GetClip> for GetClip {
+    fn from(req: crate::service::ask::GetClip) -> Self {
+        Self {
+            shortcode: req.shortcode.into_inner()
+        }
+    }
+}
+
 impl From<ShortCode> for GetClip {
     fn from(shortcode: ShortCode) -> Self {
         Self {
