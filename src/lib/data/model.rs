7a8864a28fa091fde9b8343b259ebd2ea303169f
Willian <willsbit@gmail.com>
2022-05-26 17:07:23 -0300

    [FIX] Fix compiler errors

--- src/lib/data/model.rs
+++ src/lib/data/model.rs
@@ -24,7 +24,7 @@ impl TryFrom<Clip> for crate::domain::clip::Clip {
         use std::str::FromStr;
         Ok(
             Self {
-                clip_id: field::ClipId::new(DbId::try_from(clip.clip_id.to_owned())?),
+                clip_id: field::ClipId::new(DbId::from_str(clip.clip_id.as_str())?),
                 shortcode: field::ShortCode::from(clip.shortcode.as_str()),
                 content: field::Content::new(clip.content.as_str())?,
                 title: field::Title::new(clip.title),
