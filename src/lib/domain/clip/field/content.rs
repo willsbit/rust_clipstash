d8ee90e4f33f3ca3bc3303e13d6672627f5f8171
Willian <willsbit@gmail.com>
2022-05-26 11:15:56 -0300

    [FEATURE] Implementing functionality on the content module, to ensure every clip has valid content.

---
+++ src/lib/domain/clip/field/content.rs
@@ -0,0 +1,25 @@
+use super::ClipError;
+use serde::{Deserialize, Serialize};
+use crate::domain::clip::ClipError;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Content(String);
+
+impl Content {
+    pub fn new(content: &str) -> Result<Self, ClipError> {
+        if !content.trim().is_empty() {
+            Ok(Self(content.to_owned()))
+        } else {
+            Err(ClipError::EmptyContent)
+        }
+    }
+
+    pub fn into_inner(self) -> String {
+        self.0
+    }
+
+    pub fn as_str(&self) -> &str {
+        self.0.as_str()
+    }
+}
+
