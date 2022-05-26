71c8956a5a9866824454a812f0df8fec6fda23c4
Willian <willsbit@gmail.com>
2022-05-26 20:04:37 -0300

    [FIX] Wrong variable name

--- src/lib/service/action.rs
+++ src/lib/service/action.rs
@@ -7,7 +7,7 @@ pub async fn get_clip(req: ask::GetClip, pool: &DatabasePool) -> Result<Clip, Se
     let user_pass = req.password.clone();
     let clip: Clip = query::get_clip(req, pool).await?.try_into()?;
     if clip.password.has_password() {
-        if clip.password == user_password {
+        if clip.password == user_pass {
             Ok(clip)
         } else {
             Err(ServiceError::PermissionError("Invalid password".to_owned()))
